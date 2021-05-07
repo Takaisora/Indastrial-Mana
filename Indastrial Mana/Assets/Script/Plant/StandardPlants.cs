2cb561d9f4cfc2bcf574629c4bf34598ec387798
nemonoa1021 <nemonoa1021@gmail.com>
2021-05-07 11:06:53 +0900

    生産型2と3完成

--- Indastrial Mana/Assets/Script/Plant/StandardPlants.cs
+++ Indastrial Mana/Assets/Script/Plant/StandardPlants.cs
@@ -6,15 +6,15 @@ public class StandardPlants : PlantsBase
 {
     private void Update()
     {
-        // 関数呼び出しテスト
-        //if (Input.GetMouseButtonDown(2))
-        //    base.Plant();
-        //if (Input.GetMouseButtonDown(0))
-        //    base.Watering();
-        //if (Input.GetMouseButtonDown(1))
-        //    base.fertilizing();
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //    base.Harvest();
+         //関数呼び出しテスト
+        if (Input.GetMouseButtonDown(2))
+            base.Plant();
+        if (Input.GetMouseButtonDown(0))
+            base.Watering();
+        if (Input.GetMouseButtonDown(1))
+            base.fertilizing();
+        if (Input.GetKeyDown(KeyCode.Space))
+            base.Harvest();
 
         if (base.MyGrowth == GrowthState.Planted)
             base.Growing();
