08a2b07b52536fa9bb0b5f04576e3b9dbf52b358
nakamura <yktk1023@gmail.com>
2021-05-07 18:24:34 +0900

    アイテム取得のための仮組を作成

--- Indastrial Mana/Assets/Script/Player/Playeranimation.cs
+++ Indastrial Mana/Assets/Script/Player/Playeranimation.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class Playeranimation : MonoBehaviour
 {
 
+    int seikou = 0;//デバッグ用変数
+
     float Movex = 0;//左右移動の為の変数
     float Movey = 0;//前後移動の為の変数
 
     float Move = 10;//移動量の為の変数
 
-    int direction = 0;//左右の向き判定のための変数,0=左、1=右
+    int Direction = 0;//左右の向き判定のための変数,0=左、1=右
 
     int Movenowx = 0;//いま動いているかの判断　アニメーションの有無に使用 0=立ち絵,1=移動アニメーション
     int Movenowy = 0;//いま動いているかの判断　アニメーションの有無に使用 0=立ち絵,1=移動アニメーション
+
+    int ToolState = 0;//何を所持しているかの判断 0=未所持,1=シャベル,2=バケツ,3=肥料,4=水
+
+    [HideInInspector] public bool inPlayer;
 
     Animator animator;
 
@@ -31,33 +37,30 @@ public class Playeranimation : MonoBehaviour
         Movex = 0;
         Movey = 0;
 
-        Vector3 scale = transform.localScale;
+        Vector3 scale = transform.localScale;//座標取得
 
         //左右移動
         if (Input.GetKey(KeyCode.A))
         {
             Movex = -Move;
-            // animator.SetInteger("PlayerState", 1);
             Movenowx = 1;
-            if (direction == 1)
+            if (Direction == 1)
             {
                 scale.x *= -1;
-                direction = 0;
+                Direction = 0;
             }
         } else if (Input.GetKey(KeyCode.D))
         {
-            Movex = Move;
-            // animator.SetInteger("PlayerState", 1);
+            Movex = Move;           
             Movenowx = 1;
-            if (direction == 0)
+            if (Direction == 0)
             {
                 scale.x *= -1;
-                direction = 1;
+                Direction = 1;
             }
         }
         else
         {
-            //animator.SetInteger("PlayerState", 2);
             Movenowx = 0;
         }
 
@@ -79,20 +82,54 @@ public class Playeranimation : MonoBehaviour
 
 
         //アニメーション差し替え判定
-        if(Movenowx == 1)
+        while (ToolState <2)
         {
-            animator.SetInteger("PlayerState", 1);
+            if (Movenowx == 1)
+            {
+                animator.SetInteger("PlayerState", 1);
+            }
+            else if (Movenowy == 1)
+            {
+                animator.SetInteger("PlayerState", 1);
+            }
+            else
+            {
+                animator.SetInteger("PlayerState", 2);
+            }
+            break;
         }
-        else if(Movenowy == 1)
+
+        //以下アクション
+        //道具ひろう
+        if (Input.GetKey(KeyCode.Space))
         {
-            animator.SetInteger("PlayerState", 1);
+            if (ToolState == 0)
+            {
+               if(inPlayer = true)
+                {
+                    ToolState = 1;
+                    Debug.Log("ToolState");
+                } 
+            }/*
+            else if (ToolState == 1)//肥料の上に居るかの判断,肥料をすくう
+            {
+
+            }
+            else if (ToolState == 2)//水場にいるかの判断,バケツをもって水を汲む
+            {
+
+            }
+            else if (ToolState == 3)//畑の上に居るかの判断,肥料を畑にかける
+            {
+                
+            }else if(ToolState == 4)//畑の上にいるかの判断,畑に水をやる
+            {
+
+            }*/
         }
-        else
-        {
-            animator.SetInteger("PlayerState", 2);
-        }
-            //以下更新用
-            transform.localScale = scale;
+
+        //以下更新用
+        transform.localScale = scale;
             transform.Translate(Movex / 1000, Movey / 1000, 0);
         }
     }
