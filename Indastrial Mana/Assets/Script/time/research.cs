014440d40b96b681a85bc537638474cf48be0b76
teigokou <nemonoa1021@gmail.com>
2021-05-14 09:16:18 +0900

    no message

---
+++ Indastrial Mana/Assets/Script/time/research.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class research : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int T = 3;
+        Debug.Log("Žc‚èŽžŠÔ(•b)" + (T - Time.time));
+    }
+}
