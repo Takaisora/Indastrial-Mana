5f8dfdb090840d1ff038228dfbf9c7348581bfc0
teigokou <nemonoa1021@gmail.com>
2021-06-07 12:32:14 +0900

    作業の保存

--- Indastrial Mana/Assets/Script/RandomPlant/RandomArea.cs
+++ Indastrial Mana/Assets/Script/RandomPlant/RandomArea.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class RandomArea : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+// 2Dの場合のトリガー判定
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        // 物体がトリガーに接触しとき、１度だけ呼ばれる
+        Debug.Log("エリアに入りました");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        
+        // 物体がトリガーに接触している間、常に呼ばれる
+        Debug.Log("エリア内にいます");
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        // 物体がトリガーと離れたとき、１度だけ呼ばれる
+        Debug.Log("エリアから離れました");
     }
 }
