cb8384492c7b2da3442257b92b34154cc1cd2ffc
nakamura <yktk1023@gmail.com>
2021-07-16 18:28:32 +0900

    狂気度完成

--- Indastrial Mana/Assets/Script/Map/Madnesslv5.cs
+++ Indastrial Mana/Assets/Script/Map/Madnesslv5.cs
@@ -2,16 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instace { get => _instance; }
+    static UIManager _instance;
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+    public Canvas GetParentCanvas { get => parentCanvas; }
+    [SerializeField] Canvas parentCanvas;
+}
+
 public class Madnesslv5 : Study
 {
     int positionX = 0;
     int positionY = 0;
     float MadTime = 0;
+    [SerializeField] GameObject Mad;
+    Canvas parentCanvas;
 
     // Start is called before the first frame update
     void Start()
     {
-        //gameObject.SetActive(false);
+        //parentCanvas.transform.parent = GameObject.Find("").transform;
+        parentCanvas.transform.parent = UIManager.Instace.GetParentCanvas.transform;
     }
 
     // Update is called once per frame
@@ -19,17 +35,17 @@ public class Madnesslv5 : Study
     {
         if (Madnesslv5)
         {
-            gameObject.SetActive(true);
-            positionX = Random.Range(1, 12);
-            positionY = Random.Range(1, 9);
+            positionX = Random.Range(1, 13);
+            positionY = Random.Range(1, 10);
             Vector3 SetPostion = new Vector3(Mathf.RoundToInt(positionX), Mathf.RoundToInt(positionY));
-            transform.Translate(SetPostion);
+            var temp = Instantiate(Mad, SetPostion, Quaternion.identity);
+            temp.transform.parent = parentCanvas.gameObject.transform;
             MadTime += Time.deltaTime;
             if (MadTime >= 14)
             {
                 MadTime = 0;
                 Madnesslv5 = false;
-                gameObject.SetActive(false);
+                Destroy(this.gameObject);
             }
         }
     }
